a64bb61ece10800e5c3a75d94a6400224f9aeeeb
SelamB <selambeyene2@gmail.com>
2023-12-13 18:09:24 -0800

    finishes backend setup

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from main_app import views
+
+router = routers.DefaultRouter()
+router.register(r'tests', views.TestView )
+router.register(r'results', views.ResultView )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+
 ]
--- Django/main_app/views.py
+++ Django/main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TestSerializer, ResultSerializer
+from .models import Test, Result
 
-# Create your views here.
+class TestView(viewsets.ModelViewSet):
+    serializer_class = TestSerializer
+    queryset = Test.objects.all()
+
+class ResultView(viewsets.ModelViewSet):
+    serializer_class = ResultSerializer
+    queryset = Result.objects.all()
