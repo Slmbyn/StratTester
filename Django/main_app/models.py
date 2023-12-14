45eb17c700babab5a2e53090894c40ccb2bdb80f
SelamB <selambeyene2@gmail.com>
2023-12-13 16:17:35 -0800

    updates and migrates models

--- Django/main_app/models.py
+++ Django/main_app/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 STRATEGIES = (
-    ('5min ORB'),
-    ('S/R Break'),
-    ('10/20 ema cross')
+    ('5','5min ORB'),
+    ('SR','S/R Break'),
+    ('EC','10/20 ema cross')
 )
 
 # Create your models here.
 class Test(models.Model):
-    strategy = models.CharField(choices=STRATEGIES)
-    ticker = models.CharField()
+    strategy = models.CharField(
+        max_length=2,
+        choices=STRATEGIES)
+    ticker = models.CharField(max_length=4)
     date = models.DateField()
 
     def __str__(self):
@@ -22,3 +24,6 @@ class Result(models.Model):
     entry_price = models.IntegerField()
     exit_price = models.IntegerField()
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.test.strategy},{self.PL_percent}'
