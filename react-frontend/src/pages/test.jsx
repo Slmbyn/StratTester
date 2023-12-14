b554f882c7c12ab7c552ab99478a75d58d13f915
SelamB <selambeyene2@gmail.com>
2023-12-13 21:29:34 -0800

    stubs up Test Strategy page

--- react-frontend/src/pages/test.jsx
+++ react-frontend/src/pages/test.jsx
@@ -1,6 +1,18 @@
 
 export default function Test(){
     return (
+        <>
         <h1>Test A Strategy!</h1>
+        <form action="">
+            <select name="" id="">
+                <option value="">5min ORB</option>
+                <option value="">10/20 ema cross</option>
+                <option value="">S/R Break</option>
+            </select> <br />
+            <input type="text" placeholder="Enter Stock Ticker"/><br />
+            <input type="date" /><br />
+            <button type="submit"></button>
+        </form>
+        </>
     )
 }
