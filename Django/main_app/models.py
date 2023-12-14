98c663f5284db451d2dc8337004ce193b15a872f
SelamB <selambeyene2@gmail.com>
2023-12-13 16:13:06 -0800

    creates models

---
+++ Django/main_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+STRATEGIES = (
+    ('5min ORB'),
+    ('S/R Break'),
+    ('10/20 ema cross')
+)
+
+# Create your models here.
+class Test(models.Model):
+    strategy = models.CharField(choices=STRATEGIES)
+    ticker = models.CharField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.ticker},{self.date}'
+
+class Result(models.Model):
+    PL_percent = models.IntegerField()
+    PL_abs = models.IntegerField()
+    volume = models.IntegerField()
+    entry_price = models.IntegerField()
+    exit_price = models.IntegerField()
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
