60056b8c6311da129197bebf8f6966bb901f7070
SelamB <selambeyene2@gmail.com>
2023-12-16 21:08:28 -0800

    adds some styling

--- react-frontend/src/components/newsItems.jsx
+++ react-frontend/src/components/newsItems.jsx
@@ -1,17 +1,51 @@
+import { Link } from "react-router-dom";
+import './newsItems.css';
 
-export default function NewsItem({ article }){
-    const source = article.source.name;
-    const title = article.title;
-    const articleURL = article.url;
-    const content = article.content
-    return (
-        <>
-            <h4>{title}</h4>
-            <a href={articleURL} target="_blank" rel="noopener noreferrer">
-            Read Full Article
-            </a>
-            <p>{content}</p>
+export default function NewsItem({ article }) {
+  const source = article.source.name;
+  const title = article.title;
+  const articleURL = article.url;
+  const content = article.content;
 
-        </>
-    )
+  return (
+    <div className="container dark-bg">
+      <div className="card text-bg-dark">
+        <a href={articleURL} target="_blank" rel="noopener noreferrer">
+          <div className="card-body">
+            <h4 className="card-header">{title}</h4>
+            <p className="card-text">{content}</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
+
+
+
+
+
+// import { Link } from "react-router-dom";
+
+// export default function NewsItem({ article }){
+//     const source = article.source.name;
+//     const title = article.title;
+//     const articleURL = article.url;
+//     const content = article.content
+//     return (
+//         <div className="container">
+//                 <div className="card"> 
+//                     <Link to={articleURL}>
+//                         <div className="card-body">
+//                             <h4 className="card-header">{title}</h4>
+//                             {/* <a href={articleURL} target="_blank" rel="noopener noreferrer">
+//                             Read Full Article
+//                             </a> */}
+//                             <p className="card-text">{content}</p>
+                            
+//                         </div>
+//                     </Link>
+//                 </div>
+//             </div>
+//     )
+// }
