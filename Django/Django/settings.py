bace690c8f0447ba9366043c5c40b4011ef895b4
SelamB <selambeyene2@gmail.com>
2024-05-03 15:52:13 -0700

    updates cors whitelist

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -139,7 +139,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://strat-tester.vercel.app'
 ]
 
 AUTH_USER_MODEL = 'main_app.User'
